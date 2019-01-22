4363c52eaf203627d2baa0af6a6576326df5b8c3
Markus Graube <markus.graube@tu-dresden.de>
2019-01-22 19:10:14 +0100

    add optional description for recipe step

--- src/options.ts
+++ src/options.ts
@@ -1,54 +1,63 @@
 import {ConditionType, ServiceCommand} from './enum';
 
 export interface RecipeOptions {
+    /** version of recipe */
     version: string;
+    /** name of recipe */
     name: string;
+    /** optional description of recipe */
     description?: string;
+    /** author of recipe */
     author: string;
+    /** name of initial step of recipe */
     initial_step: string;
+    /** list of recipe steps */
     steps: StepOptions[];
 }
 
 export interface StepOptions {
-    // unique name of step
+    /** unique name of step */
     name: string;
-    // array of operations performed when entering step
+    /** optional description of step */
+    description?: string;
+    /** array of operations performed when entering step */
     operations: OperationOptions[];
-    // array of transition to be checked during step
+    /** array of transition to be checked during step */
     transitions: TransitionOptions[];
 }
 
 export interface OperationOptions {
-    // module id (can be omitted if only one module is registered)
+    /** module id (can be omitted if only one module is registered) */
     module?: string;
-    // service name
+    /** service name */
     service: string;
-    // strategy name (default strategy is used if omitted)
+    /** strategy name (default strategy is used if omitted) */
     strategy?: string;
-    // command name
+    /** command name */
     command: ServiceCommand;
-    // optional parameters for start or restart
+    /** optional parameters for start or restart */
     parameter?: ParameterOptions[];
 }
 
 export interface ParameterOptions {
-    // name of dataAssembly of parameter in module JSON
+    /** name of dataAssembly of parameter in module JSON */
     name: string;
-    // variable name of dataAssembly (default: "VExt" if omitted)
+    /** variable name of dataAssembly (default: "VExt" if omitted) */
     variable?: string;
-    // value of parameter. Can be a number or a string with an expression which is interpreted using scope
+    /** value of parameter. Can be a number or a string with an expression which is interpreted using scope */
     value: string | number | boolean;
-    // scope which should be used to replace variables in value
+    /** scope which should be used to replace variables in value */
     scope?: ScopeOptions[];
-    // should parameter be continuously updated via scope (default: false)
+    /** should parameter be continuously updated via scope (default: false) */
     continuous?: boolean;
 }
 
 export interface ScopeOptions {
-    // name of variable which should be replaced in value
+    /** name of variable which should be replaced in value */
     name: string;
-    // module id (can be omitted if only one module is registered)
+    /** module id (can be omitted if only one module is registered) */
     module?: string;
+    /** service name */
     service?: string;
     strategy?: string;
     dataAssembly: string;
