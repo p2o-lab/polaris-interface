031b117be7047579aa3f89bb02bb437e07d5ab4c
Julius Lorenz <julius.lorenz@tu-dresden.de>
2022-07-28 10:03:14 +0200

    changing connection interfaces

--- src/core/connection/interfaces.ts
+++ src/core/connection/interfaces.ts
@@ -1,50 +1,26 @@
-export interface OpcUaConnectionSettings{
-  endpointUrl: string;
-  securitySettings?: OpcUaSecuritySettings;
-  authenticationSettings?: OpcUaAuthenticationSettings;
-}
 
-export interface OpcUaSecuritySettings{
-  securityMode: OpcUaMessageSecurityModes;
-  securityPolicy: OpcUaSecurityPolicies;
-}
-
-export enum OpcUaMessageSecurityModes {
-  None = 'None',
-  Sign = 'Sign',
-  SignAndEncrypt = 'SignAndEncrypt'
-}
-
-export enum OpcUaSecurityPolicies {
-  None = 'None',
-  Aes128_Sha256_RsaOaep = 'Aes128_Sha256_RsaOaep',
-  Basic128 = 'Basic128',
-  Basic128Rsa15 = 'Basic128Rsa15',
-  Basic192 = 'Basic192',
-  Basic192Rsa15 = 'Basic192Rsa15',
-  Basic256 = 'Basic256',
-  Basic256Rsa15 = 'Basic256Rsa15',
-  Basic256Sha256 = 'Basic256Sha256',
-  PubSub_Aes128_CTR = 'PubSub_Aes128_CTR',
-  PubSub_Aes256_CTR = 'PubSub_Aes256_CTR',
-}
-
-export interface OpcUaAuthenticationSettings{
-  userCredentials?: { userName: string; password: string};
-  certificate?: any;
-}
-
-export enum OpcUaAuthenticationType{
-  Anonymous = 'Anonymous',
-  UserName = 'UserName',
-  IssuedToken = 'IssuedToken',
-  Certificate =  'Certificate'
-}
-
-export interface OpcUaConnectionInfo{
-  endpointUrl: string;
+export interface OpcUaAdapterInfo{
+  id: string;
+  name?: string;
   connected: boolean;
   monitoredItemsCount: number;
-  securitySettings: OpcUaSecuritySettings;
-  authenticationSettings: OpcUaAuthenticationType;
+  currentEndpointId: string;
+  endpoints: OpcUaEndpointInfo[];
+}
+
+export interface OpcUaEndpointInfo{
+  id: string;
+  endpointUrl: string;
+  securityMode?: string;
+  securityPolicy?: string;
+}
+
+export interface OpcUaEndpointSetting{
+  endpointId: string;
+  userSettings?: OpcUaUserSetting;
+}
+
+export interface OpcUaUserSetting{
+  user: string;
+  password: string;
 }
